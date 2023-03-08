0831e664b0d45df251d439ff57b33eea2184ae2b
unknown <nguyenphuocdat18@gmail.com>
2023-03-08 17:03:33 +0700

    module2

--- module2/src/main/java/com/test/systemTime.java
+++ module2/src/main/java/com/test/systemTime.java
@@ -1,6 +1,6 @@
 package com.test;
 import java.util.Date;
-public class test1 {
+public class systemTime {
     public static void main(String[] args) {
         Date now = new Date();
         System.out.println(now);
