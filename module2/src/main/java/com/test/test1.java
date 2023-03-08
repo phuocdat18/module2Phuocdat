969e8214c2889861a96b260f82cb03058af0736b
unknown <nguyenphuocdat18@gmail.com>
2023-03-08 16:57:21 +0700

    asd

---
+++ module2/src/main/java/com/test/test1.java
@@ -0,0 +1,8 @@
+package com.test;
+import java.util.Date;
+public class test1 {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
