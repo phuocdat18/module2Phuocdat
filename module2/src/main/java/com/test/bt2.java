833c247807dc49c640af495f9cf7150e9ec01a53
unknown <nguyenphuocdat18@gmail.com>
2023-03-09 13:38:53 +0700

    bt2

---
+++ module2/src/main/java/com/test/bt2.java
@@ -0,0 +1,11 @@
+package com.test;
+import java.util.Scanner;
+
+public class bt2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
