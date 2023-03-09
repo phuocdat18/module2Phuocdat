b356db0d1bfe82c2ce3fa323c6d88934f2576c9f
unknown <nguyenphuocdat18@gmail.com>
2023-03-09 15:12:12 +0700

    tdf

---
+++ module2/src/main/java/LoopInJava/tfdfdf.java
@@ -0,0 +1,10 @@
+package LoopInJava;
+
+import java.util.Date;
+
+public class tfdfdf {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
