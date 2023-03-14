0e76c80661ac3eea9bc913a80dd19c9977b99e1b
unknown <nguyenphuocdat18@gmail.com>
2023-03-14 16:05:12 +0700

    baitap

--- module2/src/main/java/arraysAndMethodsInjava/test.java
+++ module2/src/main/java/arraysAndMethodsInjava/test.java
@@ -1,2 +1,32 @@
-package arraysAndMethodsInjava;public class test {
+package arraysAndMethodsInjava;
+
+public class test {
+    protected int sum(int a, int b) {
+       return a+b;
+   }
+   public static void main(String[] args) {
+       test d = new test();
+       int s = d.sum(10, 20);
+       System.out.println(s);
+   }
+
+//    private int sum1(int a, int b) {
+//        return a+b;
+//    }
+//    public static void main(String[] args) {
+//        test d = new test();
+//        int s = d.sum1(10, 15);
+//        System.out.println(s);
+//    }
+
+//    punlic int sum1(int a, int b) {
+//        return a+b;
+//    }
+//    public static void main(String[] args) {
+//        test d = new test();
+//        int s = d.sum1(10, 15);
+//        System.out.println(s);
+//    }
+
 }
+
--- module2/src/main/java/arraysAndMethodsInjava/test2.java
+++ module2/src/main/java/arraysAndMethodsInjava/test2.java
@@ -1,2 +1,9 @@
-package arraysAndMethodsInjava;public class test2 {
+package arraysAndMethodsInjava;
+
+public class test2 {
+    public static void main(String[] args) {
+        test d = new test();
+        int s = d.sum(5, 10);
+        System.out.println(s);
+    }
 }
